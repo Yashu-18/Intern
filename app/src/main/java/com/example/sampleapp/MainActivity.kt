57656c8866aadd46533e0310b5e6ffbb896336d7
Yashu-18 <officialyashu2022@gmail.com>
2025-02-02 03:15:10 +0530

    This is my second commit

--- app/src/main/java/com/example/sampleapp/MainActivity.kt
+++ app/src/main/java/com/example/sampleapp/MainActivity.kt
@@ -5,12 +5,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.viewpager2.widget.ViewPager2
+import com.example.sampleapp.Adapter.ViewPagerAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var Myadapter:ViewPagerAdapter
+    lateinit var viewpager:ViewPager2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+
+        Myadapter = ViewPagerAdapter(this)
+        viewpager=findViewById(R.id.viewpager)
+        viewpager.orientation=ViewPager2.ORIENTATION_VERTICAL
+        viewpager.adapter=Myadapter
     }
 }
